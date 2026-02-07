b5fe02bad473b21fa37156454eed321c4a9a9545
Mukesh-KB1 <evermukesh1@gmail.com>
2026-02-07 19:56:01 +0530

    styling sideBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router'
 
 function App() {
   return (
-    <div>App</div>
+    <div className="min-h-screen bg-[url('./assets/bgImage.svg')] bg-no-repeat bg-cover">
+      <Outlet />
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router'
+import {HomePage, LoginPage , ProfilePage} from './pages/index.js'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <App /> } >
+      <Route path="" element={ <HomePage /> } />
+      <Route path="login" element={ <LoginPage /> } />
+      <Route path="profile" element={ <ProfilePage /> } />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
